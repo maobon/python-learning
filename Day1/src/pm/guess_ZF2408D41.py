d2853cc8e52b636dacdae1d7f6346691ab2921a1
maobonhaha <maobonhaha@gmail.com>
2024-12-29 23:37:22 +0800

    day1 pm topics

--- Day1/src/pm/guess_ZF2408D41.py
+++ Day1/src/pm/guess_ZF2408D41.py
@@ -1,5 +1,25 @@
+import random
 
-
+RETRY_COUNT = 7
 
 if __name__ == '__main__':
-    print("")
+    num = random.randint(1, 100)
+    print(f"欢迎参加参数子游戏！你有{RETRY_COUNT}次机会\n")
+    # print(num)
+
+    input_num = int(input("请猜一个1到100之间的数字: "))
+
+    while num != input_num and RETRY_COUNT > 1:
+        if num > input_num:
+            print("猜的数字太小了，请再是一次！")
+            input_num = int(input("请猜一个1到100之间的数字: "))
+            RETRY_COUNT -= 1
+        elif num < input_num:
+            print("猜的数字太大了，请再是一次！")
+            input_num = int(input("请猜一个1到100之间的数字: "))
+            RETRY_COUNT -= 1
+
+    if num != input_num:
+        print(f"机会用完了，被猜的数字是{num}")
+    elif num == input_num:
+        print("恭喜，你猜对了！")
