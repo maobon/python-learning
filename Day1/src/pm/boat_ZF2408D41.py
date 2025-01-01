d2ffe1bb7217a9d8b3a41e6ff1c82881ca8781af
maobonhaha <maobonhaha@gmail.com>
2025-01-01 14:30:53 +0800

    day1 pmm

--- Day1/src/pm/boat_ZF2408D41.py
+++ Day1/src/pm/boat_ZF2408D41.py
@@ -27,15 +27,31 @@ ENGINE_STATUS_INIT = True
 class Boat:
 
     def __init__(self, curr_position_index, ammo, avi_distance, engin_status):
+        """
+        initialize the boat instance
+        :param curr_position_index: index (current position) in planet list
+        :param ammo: initial ammo amount
+        :param avi_distance: available travel distance
+        :param engin_status: engine status
+        """
         self.curr_position_index = curr_position_index
         self.ammo = ammo
         self.avi_distance = avi_distance
         self.engin_status = engin_status
 
     def set_current_position(self, curr_position_index):
+        """
+        set the current position
+        :param curr_position_index
+        """
         self.curr_position_index = curr_position_index
 
     def set_destination(self, destination_index) -> dict[str, float]:
+        """
+        user select destination index is list
+        :param destination_index:
+        :return: {destination_name, distance}
+        """
         curr_to_sun = list(destinations_arr[self.curr_position_index].values())[0]
 
         target = destinations_arr[destination_index]
@@ -46,10 +62,23 @@ class Boat:
             "distance": (abs(curr_to_sun - target_to_sun)) * CONSTANT_KM_ONE_AU
         }
 
-    # return left fuel percent
     def consume_fuel(self, index, distance: float) -> float:
+        """
+        consume fuel
+        :param index: destination index in list
+        :param distance: destination distance in km
+        :return: percentage of fuel consumed
+        """
         if distance > self.avi_distance:
             self.engin_status = False
+
+            confirm_command = input("Type yes to confirm to go destination and fuel will depletion ... \n")
+            if confirm_command == "yes":
+                self.engin_status = False
+                left_distance_to_destination = distance - self.avi_distance
+                self.avi_distance = 0
+                print(f"fuel depleted, left {left_distance_to_destination} KM to destination ...")
+
             return -1
         else:
             self.engin_status = True
@@ -58,20 +87,38 @@ class Boat:
             return (self.avi_distance / TOTAL_AVAILABLE_DISTANCE) * 100
 
     def get_curr_left_fuel_percent(self) -> str:
+        """
+        check left fuel
+        :return: percentage of left fuel
+        """
         return f"fuel left: {(self.avi_distance / TOTAL_AVAILABLE_DISTANCE) * 100:.2f} %"
 
     def get_engin_status(self) -> bool:
+        """
+        check engin status
+        :return: boolean indicating engin status
+        """
         return self.engin_status
 
     def shoot(self):
+        """
+        shoot
+        """
         self.ammo -= 1
         print("target destroyed!") if random.choice([True, False]) else print("miss...")
 
     def get_ammo(self) -> int:
+        """
+        get ammo amount
+        :return: int ammo amount
+        """
         return self.ammo
 
 
 def login():
+    """
+    ues your username and password to login
+    """
     username_retry_count = 2
     passwords_retry_count = 2
 
@@ -95,26 +142,31 @@ def login():
 
 
 if __name__ == '__main__':
+
+    # login to start game
     login()
 
     # create boat instance
     boat = Boat(POSITION_INIT, AMMO_INIT, TOTAL_AVAILABLE_DISTANCE, ENGINE_STATUS_INIT)
 
     while True:
-
         print("Select your order\n")
         for i, operation in enumerate(operations):
             print(f"{i} - {operation}")
 
         operation_index = int(input("enter your choice: "))
 
-        # go
+        # go command
         if operation_index == 0:
             if boat.get_engin_status():
                 for i, destination in enumerate(destinations_arr):
                     print(f"{i} -> {list(destination.keys())[0]}")
 
                 des_index = int(input("select your choice: "))
+                if des_index >= len(destinations_arr):
+                    print("invalid operation")
+                    continue
+
                 dis = boat.set_destination(des_index).get("distance")
 
                 fuel_left = boat.consume_fuel(des_index, dis)
@@ -125,7 +177,7 @@ if __name__ == '__main__':
             else:
                 print("no fuel available\n")
 
-        # shoot
+        # shoot command
         elif operation_index == 1:
             shoot_count = int(input("input your shoot count: "))
             if shoot_count <= 0 or shoot_count > boat.get_ammo():
@@ -136,19 +188,18 @@ if __name__ == '__main__':
 
                 print(f"left ammo:{boat.ammo}")
 
-        # fuel
+        # fuel command
         elif operation_index == 2:
             print(boat.get_curr_left_fuel_percent())
 
-
-        # motor
+        # motor command
         elif operation_index == 3:
             if boat.get_engin_status():
                 print("space ship engin status OK!")
             else:
                 print("space ship no engin status Down!")
 
-        # exit
+        # exit command
         elif operation_index == 4:
             exit(1)
 
