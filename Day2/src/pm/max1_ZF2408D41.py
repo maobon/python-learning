96bcff097d9a5976fb3d65c94920856771c56c7b
maobonhaha <maobonhaha@gmail.com>
2025-01-05 20:27:27 +0800

    day2 pm3

---
+++ Day2/src/pm/max1_ZF2408D41.py
@@ -0,0 +1,9 @@
+def max_of_two(a: int, b: int) -> int:
+    """
+    get max of two integers
+    :param a: int
+    :param b: int
+    :return: max integer
+    """
+    return max(a, b)
+
---
+++ Day2/src/pm/max2_ZF2408D41.py
@@ -0,0 +1,33 @@
+from max1_ZF2408D41 import max_of_two
+
+
+def max_of_three(a: int, b: int, c: int) -> int:
+    """
+    get max of three integers
+    :param a: int
+    :param b: int
+    :param c: int
+    :return: max integer
+    """
+    maximum = max_of_two(a, b)
+    return max_of_two(maximum, c)
+
+
+def test(*args):
+    if args is None:
+        return None
+
+    maximum = -1
+    for arg in args:
+        if arg > maximum:
+            maximum = arg
+        else:
+            continue
+
+    return maximum
+
+if __name__ == '__main__':
+    print("两个数中的最大值: ", max_of_two(10,20))
+    print("三个数中的最大值: ", max_of_three(20,40,60))
+    datas = (1,2,3,324,3242,4234,332,432,234)
+    print("一系列数中的最大值: ", test(*datas))
